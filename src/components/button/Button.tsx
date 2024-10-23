f4674105b3f4cdd8ad0098f6a009ebae5140f788
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-24 01:02:02 +0300

    feat: create a button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import s from './button.module.scss'
+import clsx from 'clsx'
+
+export type Props = {
+  variant?: 'fullWidth' | 'primary'
+} & ComponentPropsWithoutRef<'button'>
+
+export const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ className, variant = 'primary', ...restProps }, ref) => {
+    const root = clsx(s.button, s[variant], className)
+    return <button ref={ref} className={root} {...restProps}></button>
+  }
+)
