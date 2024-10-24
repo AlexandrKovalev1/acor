24445a75390d3da2527c1eb50f5aa86c4e4fca20
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-24 19:22:00 +0300

    feat: create a Logotype icon

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Eye } from './Eye'
+export { default as Close } from './Close'
+export { default as EyeOff } from './EyeOff'
+export { default as Search } from './Search'
+export { default as Logotype } from './Logotype'
