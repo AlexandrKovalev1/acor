795fb8ff50914f230f38272ecd85ff97b857836a
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-26 01:24:23 +0300

    refactor: some edits

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Router } from './router/router.tsx'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  )
+}
+
+export default App
