a0861f06f4f856cca2042492a0f35b6f940fdab4
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-25 19:25:15 +0300

    feat: added search logic to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,27 @@
 import s from './header.module.scss'
 import { TextField } from '../textField'
+import { useSearchParams } from 'react-router-dom'
+import { ChangeEvent, KeyboardEvent, useState } from 'react'
 
 export const Header = () => {
+  const [findText, setFindText] = useState('')
+  const [, setSearchParams] = useSearchParams('')
+
+  const onChangeSearchHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setFindText(e.currentTarget.value)
+    setSearchParams(searchParams => {
+      searchParams.set('search', e.currentTarget.value)
+      return searchParams
+    })
+  }
+
+  const onEnterPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      setFindText('')
+      setSearchParams('')
+    }
+  }
+
   return (
     <header className={s.header}>
       <h3 className={s.headerHeading}>Классы</h3>
@@ -12,7 +32,14 @@ export const Header = () => {
         <select name="" id="">
           <option value="">В библиотеке+1</option>
         </select>
-        <TextField type={'search'} className={s.textfield} placeholder={'Найти классы'} />
+        <TextField
+          value={findText}
+          type={'search'}
+          className={s.textfield}
+          placeholder={'Найти классы'}
+          onChange={onChangeSearchHandler}
+          onKeyUp={onEnterPressHandler}
+        />
       </div>
     </header>
   )
