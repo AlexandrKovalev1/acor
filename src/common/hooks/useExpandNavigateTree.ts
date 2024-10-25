4b1a21e1b2bbc9157c610431ba06010997c0fe1c
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-26 01:23:49 +0300

    feat: create custom hook for ExpandTree

---
+++ src/common/hooks/useExpandNavigateTree.ts
@@ -0,0 +1,38 @@
+import { TreeNode } from 'primereact/treenode'
+import { useState } from 'react'
+import { TreeExpandedKeysType } from 'primereact/tree'
+
+export const useExpandNavigateTree = (nodes: TreeNode[]) => {
+  const [expandedKeys, setExpandedKeys] = useState<TreeExpandedKeysType>({})
+
+  const expandAll = () => {
+    const _expandedKeys = {}
+
+    for (const node of nodes) {
+      expandNode(node, _expandedKeys)
+    }
+
+    setExpandedKeys(_expandedKeys)
+  }
+
+  const collapseAll = () => {
+    setExpandedKeys({})
+  }
+
+  const expandNode = (node: TreeNode, _expandedKeys: TreeExpandedKeysType) => {
+    if (node.children && node.children.length) {
+      _expandedKeys[node.key as string | number] = true
+
+      for (const child of node.children) {
+        expandNode(child, _expandedKeys)
+      }
+    }
+  }
+
+  return {
+    expandedKeys,
+    setExpandedKeys,
+    expandAll,
+    collapseAll,
+  }
+}
